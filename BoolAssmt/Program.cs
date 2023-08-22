e085878b4de5168bc38c619ad6821772f4218ef5
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-08-21 22:11:25 -0400

    Boolean Logic Assignment

---
+++ BoolAssmt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BoolAssmt
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI? true or false");
+            string yourDUI = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            string yourTicks = Console.ReadLine();
+            Console.WriteLine("Qualified?");
+            int Age = Convert.ToInt32(yourAge);
+            bool DUI = Convert.ToBoolean(yourDUI);
+            int Tickets = Convert.ToInt32(yourTicks);
+
+            if (Age > 15 && DUI == false && Tickets <= 3)
+            {
+                Console.WriteLine("true \n");
+                Main();
+            }
+            else
+            {
+                Console.WriteLine("false \n");
+                Main();
+            }
+        }
+    }
+}
