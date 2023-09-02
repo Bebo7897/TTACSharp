ffd895813a985cbff7fc4c0a318057237b9d24ad
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-09-02 18:44:27 -0400

    Named Param Assmt

---
+++ NamedParameters/NamedParam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NamedParameters
+{
+    public class NamedParam
+    {
+        public static void Display(int multiply, int display)
+        {
+            multiply = multiply * 25;
+            Console.WriteLine(display);
+        }
+    }
+}
---
+++ NamedParameters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NamedParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NamedParam myClass = new NamedParam();
+
+            Console.WriteLine("Please type an integer to multiply by 25");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please type another integer");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            NamedParam.Display(multiply: num1, display: num2);
+        }
+    }
+}
