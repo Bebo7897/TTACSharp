64116b14b1db21eddb610ffa7acd361b2fd16f48
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-09-02 09:25:45 -0400

    Calling Methods Assmt

---
+++ MethodCall/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Half
+            Console.WriteLine("Please enter the number you want to half");
+            int halfNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Half of your number is " + SimpleMath.Half(halfNum) + "\n");
+
+            //Double
+            Console.WriteLine("Please enter the number you want to double");
+            int dubNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Double your number is " + SimpleMath.Double(dubNum) + "\n");
+
+            //Square
+            Console.WriteLine("Please enter the number you want to square");
+            int sqNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number squared is " + SimpleMath.Square(sqNum) + "\n");
+        }
+    }
+}
---
+++ MethodCall/SimpleMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodCall
+{
+    class SimpleMath
+    {
+        public static int Half(int half)
+        {
+            half = half / 2;
+            return half;
+        }
+
+        public static int Double(int doubleNum)
+        {
+            doubleNum = doubleNum * 2;
+            return doubleNum;
+        }
+
+        public static int Square(int square)
+        {
+            square = square * square;
+            return square;
+        }
+    }
+}
