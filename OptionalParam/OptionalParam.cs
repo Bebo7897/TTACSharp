2bacd22e62631abfd5761a1d1e8ce992475c47a1
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-09-02 17:17:09 -0400

    Optional Param Assmt

---
+++ OptionalParam/OptionalParam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptionalParam
+{
+    class OptionalParam
+    {
+        public static int Multiple(int num1, int num2 = 1)
+        {
+            int num3 = num1 * num2;
+            return num3;
+        }
+    }
+}
---
+++ OptionalParam/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OptionalParam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Let's multiply! Maybe...\nEnter the first number for multiplication!");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+ 
+            Console.WriteLine("Enter a second number to multiply the first by or leave blank to multiply by 1");
+            string numString = Console.ReadLine();
+
+            if (numString == "")
+            {
+                Console.WriteLine("Your numbers multiplied together equals " + OptionalParam.Multiple(num1));
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(numString);
+                Console.WriteLine("Your numbers multiplied together equals " + OptionalParam.Multiple(num1,num2));
+            }
+        }
+    }
+}
