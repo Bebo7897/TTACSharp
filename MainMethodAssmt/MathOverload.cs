626062060a92a4741cb4962ad6ed7b10e1053fde
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-09-02 10:05:46 -0400

    Main Method Assmt

---
+++ MainMethodAssmt/MathOverload.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssmt
+{
+    class MathOverload
+    {
+        public static int Multiplication(int multi)
+        {
+            multi = multi * 3;
+            return multi;
+        }
+
+        public static int Multiplication(decimal multi)
+        {
+            multi = multi * 10;
+            return Convert.ToInt32(multi);
+        }
+
+        public static int Multiplication(string multi)
+        {
+            int numMulti = Convert.ToInt32(multi);
+            numMulti = numMulti * 25;
+            return numMulti;
+        }
+    }
+}
---
+++ MainMethodAssmt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MainMethodAssmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INTEGER
+            Console.WriteLine("Type an integer to multiply by 3");
+            int multThree = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your integer multiplied by 3 equals " + MathOverload.Multiplication(multThree));
+
+            //DECIMAL
+            Console.WriteLine("Type a decimal to multiply by 10");
+            decimal multTen = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Your decimal multiplied by 10 equals " + MathOverload.Multiplication(multTen));
+
+            //STRING
+            Console.WriteLine("Type a whole number to multiply by 25");
+            string multTwFive = Console.ReadLine();
+            Console.WriteLine("Your number multiplied by 25 equals " + MathOverload.Multiplication(multTwFive));
+        }
+    }
+}
