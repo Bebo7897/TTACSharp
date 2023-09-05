b588eddc11dafd9a5fbbd4cd5a5bc5d74a265a40
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-09-05 13:23:24 -0400

    Inheritance assmt

---
+++ Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", Id = 01 };
+
+            employee.SayName();
+        }
+    }
+}
