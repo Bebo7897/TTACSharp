acf21ebe5fc6955a089f4450950fe2fb628b542d
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-09-05 15:54:43 -0400

    Polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable quittable = new Employee();
+
+            quittable.Quit();
+        }
+    }
+}
