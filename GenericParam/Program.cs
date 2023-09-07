cab119ce949222f77f34734aa8004de8497ec29f
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-09-07 10:54:29 -0400

    Generic Param Assmt

---
+++ GenericParam/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericParam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.things = new List<string>() { "pen", "desk", "printer", "computer" };
+
+            Employee<int> employee1 = new Employee<int>();
+            employee1.things = new List<int>() { 5, 15, 22, 59 };
+
+            foreach (string thing in employee.things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (int thing in employee1.things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
