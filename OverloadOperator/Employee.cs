3bb3ff632d68b6be6df6c10aa0040467ce8147ec
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-09-07 10:16:11 -0400

    Overload Operator assmt

---
+++ OverloadOperator/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadOperator
+{
+    public class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int ID { get; set; }
+
+        public static bool operator== (Employee employee, Employee employee1)
+        {
+            if (employee.ID == employee1.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator!= (Employee employee, Employee employee1)
+        {
+            if (employee.ID != employee1.ID)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ OverloadOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OverloadOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { firstName = "Jon", lastName = "Snow", ID = 1 };
+            Employee employee2 = new Employee() { firstName = "Rob", lastName = "Stark", ID = 2 };
+
+            if (employee == employee2)
+            {
+                Console.WriteLine("These employees have matching IDs");
+            }
+            else
+            {
+                Console.WriteLine("These employees do not have matching IDs");
+            }
+            
+        }
+    }
+}
