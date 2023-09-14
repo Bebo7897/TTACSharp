651947095da5c8411eb6cce92b2689d82f415581
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-09-14 13:35:29 -0400

    DateTime Assmt

---
+++ DateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace DateTimeAssmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            DateTime dateTime = new DateTime();
+            dateTime = DateTime.Now;
+
+            //print Now
+            Console.WriteLine(dateTime);
+
+            //Take in Hours to add
+            Console.WriteLine("Please enter a whole number of hours to add to current time");
+            double hours = Convert.ToInt32(Console.ReadLine());
+         
+            //Print now plus hours entered
+            Console.WriteLine(dateTime.AddHours(hours));
+        }
+    }
+}
